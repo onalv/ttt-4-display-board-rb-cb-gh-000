a893477353bbf975890705219efa93c01bfef668
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-07 16:00:00 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(answers)
-  puts " #{answers[0]} |   |   "
+  puts " #{answers[0]} | #{answers[1]} | #{answers[2]} "
   puts "-----------"
-  puts "   | #{answers[4]} |   "
+  puts " #{answers[3]} | #{answers[4]} | #{answers[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{answers[6]} | #{answers[7]} | #{answers[8]} "
 end
